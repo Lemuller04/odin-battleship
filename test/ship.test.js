c8fa6ebaa9d193fe06ba451556490fa543170ed2
Leonardo <leonardomullerdelima@pm.me>
2025-07-31 16:40:42 -0300

    feat: write ship factory tests

---
+++ test/ship.test.js
@@ -0,0 +1,20 @@
+import Ship from "../src/factories/ship.js";
+
+describe("Ship factory methods", () => {
+  it("New ship isn't sunk", () => {
+    expect(Ship(2).isSunk()).toBeFalsy();
+  });
+
+  it("New ship of size two takes one hit", () => {
+    let ship = Ship(2);
+    ship.hit();
+    expect(ship.isSunk()).toBeFalsy();
+  });
+
+  it("New ship of size two sunks after two hits", () => {
+    let ship = Ship(2);
+    ship.hit();
+    ship.hit();
+    expect(ship.isSunk()).toBeTruthy();
+  });
+});
