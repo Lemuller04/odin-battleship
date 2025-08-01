7c9db57c46603605a969adb79d9c38955e8182a7
Leonardo <leonardomullerdelima@pm.me>
2025-08-01 14:04:09 -0300

    feat: newShip function doesn't create overlapped ships

--- src/factories/gameboard.js
+++ src/factories/gameboard.js
@@ -4,19 +4,85 @@ const Gameboard = () => {
   const board = {
     min: 0,
     max: 9,
+    occupied: new Set(),
   };
   const ships = [];
 
   function newShip(from, to) {
     if (invalidShape(from, to)) return false;
     if (outOfBounds(from) || outOfBounds(to)) return false;
+    if (isOccupied(from, to)) return false;
 
-    let size = from[0] !== to[0] ? from[0] - to[0] : from[1] - to[1];
-    size < 0 ? (size *= -1) : size;
+    const size = Math.abs(from[0] - to[0] || from[1] - to[1]) + 1;
 
-    ships.push(Ship(size));
+    const ship = {
+      id: ships.length,
+      ship: Ship(size),
+    };
+
+    registerOccupiedCoords(ship, from, to);
+
+    ships.push(ship);
+    board.totalShips++;
 
     return true;
+  }
+
+  function isOccupied(from, to) {
+    let [fixedNum, varyingNum, factor, fixedAxis, target] = getDiff(from, to);
+
+    while (varyingNum !== target + factor) {
+      let coord =
+        fixedAxis === "x" ? [fixedNum, varyingNum] : [varyingNum, fixedNum];
+      for (let entry of board.occupied) {
+        let [coordStr, shipIdStr] = entry.split(":");
+        if (coordStr === coord.toString()) return true;
+      }
+      varyingNum += factor;
+    }
+
+    return false;
+  }
+
+  function registerOccupiedCoords(ship, from, to) {
+    let [fixedNum, varyingNum, factor, fixedAxis, target] = getDiff(from, to);
+
+    while (varyingNum !== target + factor) {
+      let coord =
+        fixedAxis === "x" ? [fixedNum, varyingNum] : [varyingNum, fixedNum];
+      board.occupied.add(`${coord.toString()}:${ship.id}`);
+      varyingNum += factor;
+    }
+  }
+
+  function getDiff(from, to) {
+    let fixedNum;
+    let varyingNum;
+    let factor;
+    let fixedAxis;
+    let target;
+
+    if (from[0] !== to[0]) {
+      varyingNum = from[0];
+      fixedNum = from[1];
+      factor = from[0] < to[0] ? 1 : -1;
+      fixedAxis = "y";
+      target = to[0];
+    } else if (from[1] !== to[1]) {
+      varyingNum = from[1];
+      fixedNum = from[0];
+      factor = from[1] < to[1] ? 1 : -1;
+      fixedAxis = "x";
+      target = to[1];
+    } else {
+      varyingNum = from[0];
+      fixedNum = from[0];
+      factor = 0;
+      fixedAxis = "x";
+      target = to[0];
+    }
+
+    return [fixedNum, varyingNum, factor, fixedAxis, target];
   }
 
   function invalidShape(from, to) {
