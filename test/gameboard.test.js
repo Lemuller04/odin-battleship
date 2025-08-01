36799e4ce1da4f94fc9f03209d51fb7b046c4648
Leonardo <leonardomullerdelima@pm.me>
2025-08-01 14:02:03 -0300

    feat: write newShip function tests

--- test/gameboard.test.js
+++ test/gameboard.test.js
@@ -1,12 +1,17 @@
 import Gameboard from "../src/factories/gameboard.js";
 
-describe("Gameboard factory", () => {
+describe("Gameboard factory's newShip function", () => {
   it("Creates a ship", () => {
     expect(Gameboard().newShip([0, 0], [0, 1])).toBeTruthy();
+    expect(Gameboard().newShip([0, 0], [0, 9])).toBeTruthy();
+    expect(Gameboard().newShip([0, 9], [0, 0])).toBeTruthy();
+    expect(Gameboard().newShip([0, 0], [0, 0])).toBeTruthy();
+    expect(Gameboard().newShip([9, 9], [9, 9])).toBeTruthy();
   });
 
   it("Doesn't create invalid shaped ships", () => {
     expect(Gameboard().newShip([0, 0], [1, 1])).toBeFalsy();
+    expect(Gameboard().newShip([9, 0], [8, 9])).toBeFalsy();
   });
 
   it("Doesn't create out of bounds ships", () => {
@@ -18,5 +23,13 @@ describe("Gameboard factory", () => {
     expect(Gameboard().newShip([0, 10], [0, 0])).toBeFalsy();
     expect(Gameboard().newShip([-1, 0], [0, 0])).toBeFalsy();
     expect(Gameboard().newShip([0, -1], [0, 0])).toBeFalsy();
+    expect(Gameboard().newShip([-1, -1], [-1, -1])).toBeFalsy();
+  });
+
+  it("Doens't create ovelapped ships", () => {
+    let gb = Gameboard();
+    gb.newShip([0, 0], [0, 4]);
+    expect(gb.newShip([0, 0], [4, 0])).toBeFalsy();
+    expect(gb.newShip([9, 4], [0, 4])).toBeFalsy();
   });
 });
