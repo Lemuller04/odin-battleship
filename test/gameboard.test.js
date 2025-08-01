3bfa473c1919ef14367e865ba14f287c52f31413
Leonardo <leonardomullerdelima@pm.me>
2025-08-01 10:46:34 -0300

    feat: write newShip function tests

--- test/gameboard.test.js
+++ test/gameboard.test.js
@@ -8,4 +8,15 @@ describe("Gameboard factory", () => {
   it("Doesn't create invalid shaped ships", () => {
     expect(Gameboard().newShip([0, 0], [1, 1])).toBeFalsy();
   });
+
+  it("Doesn't create out of bounds ships", () => {
+    expect(Gameboard().newShip([0, 0], [0, 10])).toBeFalsy();
+    expect(Gameboard().newShip([0, 0], [10, 0])).toBeFalsy();
+    expect(Gameboard().newShip([0, 0], [0, -1])).toBeFalsy();
+    expect(Gameboard().newShip([0, 0], [-1, 0])).toBeFalsy();
+    expect(Gameboard().newShip([10, 0], [0, 0])).toBeFalsy();
+    expect(Gameboard().newShip([0, 10], [0, 0])).toBeFalsy();
+    expect(Gameboard().newShip([-1, 0], [0, 0])).toBeFalsy();
+    expect(Gameboard().newShip([0, -1], [0, 0])).toBeFalsy();
+  });
 });
