debeaaed9e0d6bea0d70ab7e7a8f7faf672be488
Leonardo <leonardomullerdelima@pm.me>
2025-08-02 18:42:04 -0300

    feat: extract traversePath function on gameboard factory to reduce repetition

--- src/factories/gameboard.js
+++ src/factories/gameboard.js
@@ -5,54 +5,80 @@ const Gameboard = () => {
     min: 0,
     max: 9,
     occupied: new Set(),
+    totalShips: 0,
+    sunkenShips: 0,
+    attacked: new Set(),
   };
   const ships = [];
+
+  function receiveAttack(coord) {
+    let shipId;
+    console.log(board.occupied);
+    for (let entry of board.occupied) {
+      console.log("aljsdklajsdlajs");
+    }
+    for (let ship of ships) {
+      if (ship.id == shipId) {
+        ship.hit();
+        if (ship.isSunk()) board.sunkenShips++;
+        return "ship";
+      }
+    }
+    board.attacked.add(coord.toString());
+    return "water";
+  }
 
   function newShip(from, to) {
     if (invalidShape(from, to)) return false;
     if (outOfBounds(from) || outOfBounds(to)) return false;
-    if (isOccupied(from, to)) return false;
 
     const size = Math.abs(from[0] - to[0] || from[1] - to[1]) + 1;
 
     const ship = {
       id: ships.length,
       ship: Ship(size),
+      path: traversePath(from, to),
     };
 
-    registerOccupiedCoords(ship, from, to);
+    if (isOccupied(ship.path)) return false;
 
+    registerOccupiedCoords(ship);
     ships.push(ship);
     board.totalShips++;
 
     return true;
   }
 
-  function isOccupied(from, to) {
-    let [fixedNum, varyingNum, factor, fixedAxis, target] = getDiff(from, to);
-
-    while (varyingNum !== target + factor) {
-      let coord =
-        fixedAxis === "x" ? [fixedNum, varyingNum] : [varyingNum, fixedNum];
-      for (let entry of board.occupied) {
-        let [coordStr, shipIdStr] = entry.split(":");
-        if (coordStr === coord.toString()) return true;
+  function isOccupied(path) {
+    for (let cell of board.occupied) {
+      for (let coord of path) {
+        if (coord.toString() === cell.split(":")[1]) {
+          return true;
+        }
       }
-      varyingNum += factor;
     }
-
     return false;
   }
 
-  function registerOccupiedCoords(ship, from, to) {
+  // Register the occupied cells using shipdId:coord format
+  function registerOccupiedCoords(ship) {
+    for (let coord of ship.path) {
+      board.occupied.add(`${ship.id}:${coord}`);
+    }
+  }
+
+  function traversePath(from, to) {
     let [fixedNum, varyingNum, factor, fixedAxis, target] = getDiff(from, to);
+    let path = [];
 
     while (varyingNum !== target + factor) {
       let coord =
         fixedAxis === "x" ? [fixedNum, varyingNum] : [varyingNum, fixedNum];
-      board.occupied.add(`${coord.toString()}:${ship.id}`);
+      path.push(coord);
       varyingNum += factor;
     }
+
+    return path;
   }
 
   function getDiff(from, to) {
@@ -98,6 +124,7 @@ const Gameboard = () => {
 
   return {
     newShip,
+    receiveAttack,
   };
 };
 
